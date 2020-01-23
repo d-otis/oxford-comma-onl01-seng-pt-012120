16b8512e01ebd0a01a63d19e45955caf13091702
Dan Foley <dan@dan-foley.com>
2020-01-23 22:47:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ def oxford_comma(array)
   # pop last element off of the array + save that into a var
   # join remaining array with commas
   # add last element again with proper English syntax
+  comma_and = ", and "
   last_element = array.pop
   array.join(', ')
-  
+  array.push(comma_and)
 end
