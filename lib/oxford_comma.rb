a8dd50fdeba15b3b7547386705394946e6a63b6b
Dan Foley <dan@dan-foley.com>
2020-01-23 23:04:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,17 +3,16 @@ require "pry"
 ex = ["kiwi", "durian", "starfruit"]
 
 def oxford_comma(array)
-  # pop last element off of the array + save that into a var
-  # join remaining array with commas
-  # add last element again with proper English syntax
   if array.size == 1
     array.join
   elsif array.size == 2
     array.join(" and ")
   else
+    # pop last element off of the array + save that into a var
+    # join remaining array with commas
+    # add last element again with proper English syntax
     comma_and = ", and"
     last_element = array.pop
-    array.join(", ")
     # binding.pry
     array.push(comma_and)
     array.push(last_element)
